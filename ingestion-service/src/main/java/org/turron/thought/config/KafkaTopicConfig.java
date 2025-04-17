c3f6968569fc6ba5744688b11a1640cec2161031
Fl1s <maksima7th@gmail.com>
2025-04-17 23:26:53 +0700

    fix: discovery client support, kafka configuration

---
+++ ingestion-service/src/main/java/org/turron/thought/config/KafkaTopicConfig.java
@@ -0,0 +1,21 @@
+package org.turron.ingestion.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic thoughtStatusTopic() {
+        return TopicBuilder.name("thought-status")
+                .build();
+    }
+
+    @Bean
+    public NewTopic ingestedThoughtTopic() {
+        return TopicBuilder.name("ingested-thought")
+                .build();
+    }
+}

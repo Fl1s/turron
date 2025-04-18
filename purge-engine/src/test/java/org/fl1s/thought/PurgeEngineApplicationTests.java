758d7bf81f44ffbd1145565499a45ef5c16d80b4
Fl1s <maksima7th@gmail.com>
2025-04-18 11:16:29 +0700

    feat: purge-engine init

---
+++ purge-engine/src/test/java/org/fl1s/thought/PurgeEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fl1s.thought;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurgeEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

275a0541bcd8ce43f7d6be1bb9f7c6ce5dc17981
Fl1s <maksima7th@gmail.com>
2025-04-18 23:15:00 +0700

    fix: build.gradle & some pipeline lines

--- eureka-server/src/main/java/org/turron/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/turron/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package org.turron.eurekaserver;
+package org.turron.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

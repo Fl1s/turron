b05ce7e5e7f463c7be2da8c83359f17ab9e55a70
Fl1s <maksima7th@gmail.com>
2025-06-22 01:37:53 +0700

    refactor: search-service now returns pree-signed url instead of rushing video into the rest api

--- search-service/src/main/java/org/turron/service/service/MinioService.java
+++ search-service/src/main/java/org/turron/service/service/MinioService.java
@@ -2,6 +2,9 @@ package org.turron.service.service;
 
 import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.http.Method;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -52,6 +55,32 @@ public class MinioService {
         } catch (Exception e) {
             log.error("Failed to download video from MinIO", e);
             throw new RuntimeException("Failed to download video from MinIO", e);
+        }
+    }
+
+    /**
+     * Generates a pre-signed URL for accessing an object in MinIO.
+     *
+     * The URL provides temporary public access to the specified object using HTTP GET,
+     * valid for 1 hour (3600 seconds).
+     *
+     * @param objectName the name of the object in the bucket
+     * @return a pre-signed URL string to access the object
+     * @throws RuntimeException if URL generation fails
+     */
+    public String generatePreSignedUrl(String objectName) {
+        try {
+            return minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs.builder()
+                            .bucket(uploadsBucket)
+                            .object(objectName)
+                            .method(Method.GET)
+                            .expiry(3600, TimeUnit.SECONDS)
+                            .build()
+            );
+        } catch (Exception e) {
+            log.error("Failed to generate pre-signed URL for {}", objectName, e);
+            throw new RuntimeException("Could not generate pre-signed URL", e);
         }
     }
 }

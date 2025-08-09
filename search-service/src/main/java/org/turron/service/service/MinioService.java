8b7a83004303dc3d569a3482944c37bf89696792
Fl1s <maksima7th@gmail.com>
2025-08-10 01:23:14 +0700

    hotfix: minio presigned-urls accessibility

--- search-service/src/main/java/org/turron/service/service/MinioService.java
+++ search-service/src/main/java/org/turron/service/service/MinioService.java
@@ -1,6 +1,5 @@
 package org.turron.service.service;
 
-import io.minio.GetObjectArgs;
 import io.minio.MinioClient;
 import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.http.Method;
@@ -9,10 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 
 /**
  * Service for interacting with MinIO object storage.
@@ -29,36 +24,6 @@ public class MinioService {
     private String uploadsBucket;
 
     /**
-     * Downloads a video file from the MinIO bucket using the given source ID.
-     *
-     * @param sourceId the ID of the source video to download
-     * @return a {@link File} object pointing to a temporary file containing the downloaded video
-     * @throws RuntimeException if the video cannot be downloaded
-     */
-    public File downloadVideo(String sourceId) {
-        String objectName = "sources/" + sourceId;
-        log.info("Downloading video from MinIO: bucket='{}', object='{}'", uploadsBucket, objectName);
-
-        try {
-            File tempFile = File.createTempFile("video-", ".mp4");
-
-            try (InputStream in = minioClient.getObject(GetObjectArgs.builder()
-                    .bucket(uploadsBucket)
-                    .object(objectName)
-                    .build());
-                 FileOutputStream out = new FileOutputStream(tempFile)) {
-
-                in.transferTo(out);
-                log.info("Video downloaded to temporary file: {}", tempFile.getAbsolutePath());
-                return tempFile;
-            }
-        } catch (Exception e) {
-            log.error("Failed to download video from MinIO", e);
-            throw new RuntimeException("Failed to download video from MinIO", e);
-        }
-    }
-
-    /**
      * Generates a pre-signed URL for accessing an object in MinIO.
      *
      * The URL provides temporary public access to the specified object using HTTP GET,
@@ -70,7 +35,7 @@ public class MinioService {
      */
     public String generatePreSignedUrl(String objectName) {
         try {
-            String url = minioClient.getPresignedObjectUrl(
+            return minioClient.getPresignedObjectUrl(
                     GetPresignedObjectUrlArgs.builder()
                             .bucket(uploadsBucket)
                             .object(objectName)
@@ -78,7 +43,6 @@ public class MinioService {
                             .expiry(3600, TimeUnit.SECONDS)
                             .build()
             );
-            return url.replace("minio", "localhost");
         } catch (Exception e) {
             log.error("Failed to generate pre-signed URL for {}", objectName, e);
             throw new RuntimeException("Could not generate pre-signed URL", e);
